de413eda68ff61a15de81263baea867e43af325a
hashlyschool <ilya12-141998@mail.ru>
2022-05-27 15:23:40 +0300

    added finish cpp05 ex00

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -3,8 +3,8 @@
 int	main()
 {
 	{
-		Bureaucrat	first("First", 75);
-		Bureaucrat	second("Second", 75);
+		Bureaucrat	first("First", 15);
+		Bureaucrat	second("Second", 125);
 
 		std::cout << first << std::endl;
 		std::cout << second << std::endl;
@@ -19,8 +19,8 @@ int	main()
 		{
 			std::cout << exc.what();
 		}
+		std::cout << std::endl << std::endl;
 	}
-	std::cout << std::endl << std::endl;
 	{
 		try
 		{
@@ -30,18 +30,54 @@ int	main()
 		{
 			std::cout << exc.what();
 		}
+		std::cout << std::endl << std::endl;
 	}
-	std::cout << std::endl << std::endl;
 	{
+		Bureaucrat	test("One", 1);
 		try
 		{
-			Bureaucrat plus("Plus", 250);
+			test.incrementGrade();
 		}
 		catch (Bureaucrat::Exception &exc)
 		{
 			std::cout << exc.what();
 		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl << std::endl;
+	std::cout << std::endl;
+	{
+		Bureaucrat	test("Second", 150);
+		try
+		{
+			test.decrementGrade();
+		}
+		catch (Bureaucrat::Exception &exc)
+		{
+			std::cout << exc.what();
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		Bureaucrat	test("blabla", 15);
+		try
+		{
+			test.decrementGrade();
+		}
+		catch (Bureaucrat::Exception &exc)
+		{
+			std::cout << exc.what();
+		}
+		std::cout << test << std::endl;
+		try
+		{
+			test.incrementGrade();
+		}
+		catch (Bureaucrat::Exception &exc)
+		{
+			std::cout << exc.what();
+		}
+		std::cout << test << std::endl;
+	}
 	return (0);
 }
